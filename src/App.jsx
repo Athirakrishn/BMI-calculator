698548fa138d0d4aeb83b10a1314883d88322bbb
Athirakrishn <athiraramakrishnan41020@gmail.com>
2025-07-25 11:25:07 +0530

    part-3

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,23 @@ function App() {
   return (
     <>
     <div className="container md-5 justify-content-center text-align-center">
-      <div className="container " style={{height:"450px",width:"400px", backgroundColor:"grey"}}>
+      <div className="container " style={{height:"450px",width:"400px", backgroundColor:"gray"}}>
         <input type="text"  className='text-center text-align-center p-2 mt-3 ' style={{width:"100%"}}/>
+        <div className="row mt-3">
+          <div className="col-2 m-2 btn btn-outline-dark text-light shadow">9</div>
+          <div className="col-2 m-2 btn btn-outline-dark text-light shadow">8</div>
+          <div className="col-2 m-2 btn btn-outline-dark text-light shadow">7</div>
+        </div>
+         <div className="row">
+          <div className="col-2 m-2 btn btn-outline-dark text-light shadow">6</div>
+          <div className="col-2 m-2 btn btn-outline-dark text-light shadow">5</div>
+          <div className="col-2 m-2 btn btn-outline-dark text-light shadow">4</div>
+        </div>
+         <div className="row">
+          <div className="col-2 m-2 btn btn-outline-dark text-light shadow">3</div>
+          <div className="col-2 m-2 btn btn-outline-dark text-light shadow">2</div>
+          <div className="col-2 m-2 btn btn-outline-dark text-light shadow">1</div>
+        </div>
         
       </div>
     </div>
